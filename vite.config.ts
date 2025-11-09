92d30ad1dd3e5a08c4e082f19026179a38fa2556
akash-aanand <akashanandmaurya@gmail.com>
2025-11-09 11:37:21 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
     fs: {
-      allow: ["./client", "./shared"],
+      allow: ["./",
+
+"./client", "./shared"],
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
